a490e60705b25415c0748c220f90bfe0b687c4bb
Ziming Weng <luojiuoscar@gmail.com>
2025-10-29 18:21:13 +0800

    added a void dimension with EmptyChunkGenerator

--- src/main/java/net/luojiuoscar/isaac_disaster/worldgen/dimension/ModDimensions.java
+++ src/main/java/net/luojiuoscar/isaac_disaster/worldgen/dimension/ModDimensions.java
@@ -2,6 +2,7 @@ package net.luojiuoscar.isaac_disaster.worldgen.dimension;
 
 import com.mojang.datafixers.util.Pair;
 import net.luojiuoscar.isaac_disaster.IsaacDisaster;
+import net.luojiuoscar.isaac_disaster.worldgen.dynamic.EmptyChunkGenerator;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
@@ -17,63 +18,53 @@ import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
 import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.dimension.LevelStem;
-import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
-import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 
 import java.util.List;
 import java.util.OptionalLong;
 
 public class ModDimensions {
     public static final ResourceKey<LevelStem> ISAACDIM_KEY = ResourceKey.create(Registries.LEVEL_STEM,
-            ResourceLocation.fromNamespaceAndPath(IsaacDisaster.MOD_ID, "isaacdim"));
+            ResourceLocation.fromNamespaceAndPath(IsaacDisaster.MOD_ID, "fissure"));
     public static final ResourceKey<Level> ISAACDIM_LEVEL_KEY = ResourceKey.create(Registries.DIMENSION,
-            ResourceLocation.fromNamespaceAndPath(IsaacDisaster.MOD_ID, "isaacdim"));
+            ResourceLocation.fromNamespaceAndPath(IsaacDisaster.MOD_ID, "fissure"));
     public static final ResourceKey<DimensionType> ISAACDIM_DIM_TYPE = ResourceKey.create(Registries.DIMENSION_TYPE,
-            ResourceLocation.fromNamespaceAndPath(IsaacDisaster.MOD_ID, "isaacdim_type"));
-
+            ResourceLocation.fromNamespaceAndPath(IsaacDisaster.MOD_ID, "fissure_type"));
 
     public static void bootstrapType(BootstapContext<DimensionType> context) {
         context.register(ISAACDIM_DIM_TYPE, new DimensionType(
-                OptionalLong.of(12000), // fixedTime
-                false, // hasSkylight
-                false, // hasCeiling
-                false, // ultraWarm
-                false, // natural
-                1.0, // coordinateScale
-                true, // bedWorks
-                false, // respawnAnchorWorks
-                0, // minY
-                256, // height
-                256, // logicalHeight
-                BlockTags.INFINIBURN_OVERWORLD, // infiniburn
-                BuiltinDimensionTypes.OVERWORLD_EFFECTS, // effectsLocation
-                1.0f, // ambientLight
-                new DimensionType.MonsterSettings(false, false, ConstantInt.of(0), 0)));
+                OptionalLong.of(12000),
+                false,
+                false,
+                false,
+                false,
+                1.0,
+                true,
+                false,
+                0,
+                256,
+                256,
+                BlockTags.INFINIBURN_OVERWORLD,
+                BuiltinDimensionTypes.OVERWORLD_EFFECTS,
+                0.0f,
+                new DimensionType.MonsterSettings(false, false, ConstantInt.of(0), 0)
+        ));
     }
 
     public static void bootstrapStem(BootstapContext<LevelStem> context) {
         HolderGetter<Biome> biomeRegistry = context.lookup(Registries.BIOME);
         HolderGetter<DimensionType> dimTypes = context.lookup(Registries.DIMENSION_TYPE);
-        HolderGetter<NoiseGeneratorSettings> noiseGenSettings = context.lookup(Registries.NOISE_SETTINGS);
 
-        NoiseBasedChunkGenerator noiseBasedChunkGenerator = new NoiseBasedChunkGenerator(
-                MultiNoiseBiomeSource.createFromList(
-                        new Climate.ParameterList<>(List.of(
-                                Pair.of(
-                                        Climate.parameters(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F),
-                                        biomeRegistry.getOrThrow(Biomes.PLAINS)
-                                )
-                        ))
-                ),
-                noiseGenSettings.getOrThrow(NoiseGeneratorSettings.OVERWORLD) // 使用主世界地形生成逻辑
-        );
+        MultiNoiseBiomeSource biomeSource = MultiNoiseBiomeSource.createFromList(
+                new Climate.ParameterList<>(List.of(
+                        Pair.of(
+                                Climate.parameters(0f,0f,0f,0f,0f,0f,0f),
+                                biomeRegistry.getOrThrow(Biomes.THE_VOID)
+                        )
+                )));
 
-        LevelStem stem = new LevelStem(dimTypes.getOrThrow(ModDimensions.ISAACDIM_DIM_TYPE), noiseBasedChunkGenerator);
+        EmptyChunkGenerator emptyGen = new EmptyChunkGenerator(biomeSource);
 
+        LevelStem stem = new LevelStem(dimTypes.getOrThrow(ISAACDIM_DIM_TYPE), emptyGen);
         context.register(ISAACDIM_KEY, stem);
     }
-
-
-
-
 }
